36e6ac5a9c36fd89b5c8a3c929dde0d7d671ce35
Simon Oczadly <oczadly@otris.de>
2025-04-24 10:58:41 +0200

    indent blankline

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      require("ibl").setup({
+        indent = {
+          char = "┊", -- or try "┊", "¦", "▏"
+        },
+        scope = {
+          enabled = true,
+          show_start = true,
+          show_end = true,
+          highlight = { "Function", "Label" }, -- tweak colors
+        },
+      })
+    end
+  }
+}
